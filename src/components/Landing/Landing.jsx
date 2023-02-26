9d49ba3c191c1e7aa1692bad32352efa17bed154
Tadeo Marino <100310429+tadeomarino@users.noreply.github.com>
2023-02-25 23:55:34 -0300

    Agregando page-header

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -4,6 +4,7 @@ import NavBar from '../NavBar/NavBar'
 import ItemListContainer from "../ItemListContainer/ItemListContainer"
 // import ItemCount from '../ItemCount/ItemCount'
 
+const pathImgUrl = require.context("../../assets/img", true);
 
 const Landing = () => {
   
@@ -12,6 +13,11 @@ const Landing = () => {
     <div className='app-container'>
       
       <NavBar/>
+      
+      <div>
+        <img className="page-header" src={pathImgUrl("./Nike-header.jpg")} alt="" />
+      </div>
+      
       <ItemListContainer greeting="ItemListContainer"/>
       {/* <ItemCount initial={1} stock={10} onAdd={handleAddToCart}/> */}
     
