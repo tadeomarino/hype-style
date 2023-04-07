cc8688e0589adcc851932816905eeda7a04dee1d
Tadeo Marino <100310429+tadeomarino@users.noreply.github.com>
2023-04-07 00:11:08 -0300

    Fixing categories

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -37,7 +37,7 @@ const ItemListContainer = () => {
       } else {
         setShouldFilter(false);
       }
-    }, [categoryId]);
+    }, [categoryId, items]);
   
     const products = shouldFilter ? filteredProductList : items;
 
